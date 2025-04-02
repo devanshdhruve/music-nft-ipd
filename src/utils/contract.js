f944c2ed902caeb60553e92960c57337ad185859
Devansh Dhruve <dhruvedevansh@gmail.com>
2025-04-02 18:29:01 +0530

    resuming the project

--- src/utils/contract.js
+++ src/utils/contract.js
@@ -1,7 +1,7 @@
 import { ethers } from "ethers";
 import contractABI from "../../artifacts/contracts/MusicNFTMarketplace.sol/MusicNFTMarketplace.json" assert { type: "json" };
 
-const contractAddress = "0xdaeFaB09d327864cABb5C211f0ef1B03cf67BE36"; // Ensure this is correct
+const contractAddress = "0xE5aD3CC39D9bf1F5C3b26C78fC96dC9420AFa40B"; // Ensure this is correct
 
 export const getContract = (providerOrSigner) => {
   if (!providerOrSigner) throw new Error("Provider or Signer is required");
